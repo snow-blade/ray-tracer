e812aa5c9a627875106df5527fbc1b4918b73679
snow-blade <freemanirabaruta@gmail.com>
2021-05-02 17:09:28 +0000

    rename files for a better code view

--- ray.hpp
+++ ray.hpp
@@ -1,17 +1,17 @@
 #ifndef RAYH
 #define RAYH
-#include "vec3.h"
+#include "vec3.hpp"
 
 class ray{
  public: 
     ray() {}
-    ray(const vec3& v1, const vec3& v2) const {A=v1; B=v2;}
+    ray(const vec3& a, const vec3& b) {A=a; B=b;}
     vec3 origin() const {return A;}
     vec3 direction() const {return B;}
-    vec3 point_at_parameter(float t ) const {return A + t*B; }
-    
+    vec3 point_at_parameter(float t ) const {return A + B*t; }    
     vec3 A;
     vec3 B;
+
 };
 
 #endif // !RAYH
