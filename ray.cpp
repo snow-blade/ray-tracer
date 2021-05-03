a9154e29ea47515976436d8526c109c5f7a6541b
snow-blade <freemanirabaruta@gmail.com>
2021-05-03 21:17:18 +0000

    now i'm adding surfaces to my ray-tracer

--- ray.cpp
+++ ray.cpp
@@ -1,22 +1,30 @@
 #include <iostream>
 #include "ray.hpp"
 
-bool hit_sphere(const vec3& center,float radius,const ray &r){
+float hit_sphere(const vec3& center,float radius,const ray &r){
   vec3 oc = r.origin() - center;
   float a = dot(r.direction(),r.direction());
   float b = 2.0 * dot(oc,r.direction());
   float c = dot(oc,oc) - radius*radius;
   float delta = b*b - 4*a*c;
-  return delta > 0;
-
+  if (delta < 0){
+    return -1.0;
+  }
+  else {
+    return (-b - sqrt(delta)) / (2.0 * a);
+  }
 }
 vec3 color(const ray &r){
-    if(hit_sphere(vec3(0.0,0.2,-1.0),0.5,r))
-       return vec3(1.0,0.0,0.0);
+   float t = hit_sphere(vec3(0,0,-1),0.5,r);
+    if(t > 0.0) {
+      vec3 N = unit_vector(r.point_at_parameter(t) - vec3(0,0,-1));
+      return vec3(N.x()+1, N.y()+1,N.z()+1)*0.5;
+    }
     vec3 unit_direction = unit_vector(r.direction());
-    float t = 0.5 * (unit_direction.y()+1.0);
+     t = 0.5 * (unit_direction.y()+1.0);
     return vec3(1.0,1.0,1.0)*(1.0-t) + vec3(0.5,0.7,1.0)*t ;
 }
+
 /* The ​color(ray)​ function linearly blends white and blue depending on the up/downess of the y
 coordinate.
 I first made it a unit vector so -1.0 < ​y​ < 1.0. I then did a standard graphics trick of
