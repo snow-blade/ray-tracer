a0019725e01d53ed9e700ba821ff0786e16973ed
snow-blade <freemanirabaruta@gmail.com>
2021-05-04 21:52:42 +0000

    comment the unused code for a later use(maybe?)

--- ray.cpp
+++ ray.cpp
@@ -1,19 +1,21 @@
 #include <iostream>
 #include "ray.hpp"
 
-float hit_sphere(const vec3& center,float radius,const ray &r){
-  vec3 oc = r.origin() - center;
-  float a = dot(r.direction(),r.direction());
-  float b = 2.0 * dot(oc,r.direction());
-  float c = dot(oc,oc) - radius*radius;
-  float delta = b*b - 4*a*c;
-  if (delta < 0){
-    return -1.0;
-  }
-  else {
-    return (-b - sqrt(delta)) / (2.0 * a);
-  }
-}
+// float hit_sphere(const vec3& center,float radius,const ray &r){
+//   vec3 oc = r.origin() - center;
+//   float a = dot(r.direction(),r.direction());
+//   float b = 2.0 * dot(oc,r.direction());
+//   float c = dot(oc,oc) - radius*radius;
+//   float delta = b*b - 4*a*c;
+//   if (delta < 0){
+//     return -1.0;
+//   }
+//   else {
+//     return (-b - sqrt(delta)) / (2.0 * a);
+//   }
+// }
+
+
 vec3 color(const ray &r){
    float t = hit_sphere(vec3(0,0,-1),0.5,r);
     if(t > 0.0) {
