685dead2164680184dfd16d4be7c8c554c0b1235
snow-blade <freemanirabaruta@gmail.com>
2021-05-05 23:53:27 +0000

    create a new camera header file

---
+++ camera.hpp
@@ -0,0 +1,20 @@
+#ifndef CAMERAH
+#define CAMERAH
+#include "ray.hpp"
+
+class camera {
+    public:
+     camera() {
+   lower_left_corner =vec3(-2.0,-1.0,-1.0);
+   horizontal=vec3(4.0,0.0,0.0);
+   vertical=vec3(0.0,2.0,0.0);
+   origin=vec3(0.0,0.0,0.0); 
+     }
+     ray get_ray(float u,float v) {return ray(origin,lower_left_corner+horizontal*u+vertical*v);}
+     vec3 origin;
+     vec3 lower_left_corner;
+     vec3 horizontal;
+     vec3 vertical;
+}
+
+#endif // !CAMERAH
